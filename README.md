5219425b7ad2a4447930d74c97184e813cdc84bb
Richard W. Norton <rwtnorton@gmail.com>
2013-06-15 09:50:22 -0500

    Initial commit.  NI!

---
+++ README.md
@@ -0,0 +1,30 @@
+# Knights::Who::Say::Ni
+
+Counts the number of legal positions of two opposing knights on
+an N x M chess board.
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+    gem 'knights-who-say-ni'
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install knights-who-say-ni
+
+## Usage
+
+TODO: Write usage instructions here
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
---
+++ lib/knights_who_say_ni.rb
@@ -0,0 +1,21 @@
+# knights-who-say-ni:  Counts number of non-capture positions of
+#  two opposing knights on an NxM chess board.
+# Copyright (C) 2013  Richard W. Norton
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'knights_who_say_ni/version'
+
+module KnightsWhoSayNi
+end
