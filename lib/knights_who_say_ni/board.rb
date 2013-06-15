43c94307068c536319084270ef468e8b04b64cb9
Richard W. Norton <rwtnorton@gmail.com>
2013-06-15 13:00:18 -0500

    Promoted *_bounds to attrs.
    Ditched the bounds method because it is not being used.

--- lib/knights_who_say_ni/board.rb
+++ lib/knights_who_say_ni/board.rb
@@ -1,11 +1,13 @@
 module KnightsWhoSayNi
   class Board
 
-    attr_reader :width, :height
+    attr_reader :width, :height, :width_bounds, :height_bounds
 
     def initialize(args={})
       @width = args.fetch(:width)
       @height = args.fetch(:height)
+      @width_bounds = 0 .. (width - 1)
+      @height_bounds = 0 .. (height - 1)
     end
 
     def self.[](width, height)
@@ -18,18 +20,6 @@ module KnightsWhoSayNi
 
     def to_a
       [width, height]
-    end
-
-    def width_bounds
-      0 .. (width - 1)
-    end
-
-    def height_bounds
-      0 .. (height - 1)
-    end
-
-    def bounds
-      [width_bounds, height_bounds]
     end
 
     def cover?(position)
